e0f128a391ac4ea320ed1c7a29d4f6a64a641d28
Alanda Nunes Ghiraldello <81779558+AlandaGhiraldello@users.noreply.github.com>
2022-05-03 20:29:03 -0300

    Remoção e adição do bootstrap para funcionar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormPersonalComponent } from './core/form-personal/form-personal.component';
-import { FormAccountComponent } from '.core/form-account/form-account.component';
-import { FeedComponent } from './core/feed/feed.component';
-import { MenuComponent } from './core/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FormPersonalComponent,
-    FormAccountComponent,
-    FeedComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
